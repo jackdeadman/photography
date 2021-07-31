038049e35d8648e9fbdd3a443270f46cdd872146
jackdeadman1@gmail.com <jackdeadman1@gmail.com>
2021-07-31 23:27:17 +0100

    Caching

--- src/routes/api/photos.json.js
+++ src/routes/api/photos.json.js
@@ -1,7 +1,10 @@
 import db from '$lib/database';
 
-export async function get({params}) {
+export async function get() {
     return await {
-        body: await db.photos()
+        body: await db.photos(),
+        headers: {
+            'cache-control': 'max-age=120'
+        }
     };
 }
