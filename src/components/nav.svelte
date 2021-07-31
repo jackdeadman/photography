965e2306b95d74c4040f624636df70740944f9a2
jackdeadman1@gmail.com <jackdeadman1@gmail.com>
2021-08-01 00:46:50 +0100

    Socials

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,12 +1,38 @@
-<h1><a href="/"><div class="me"></div><span>Jack&rsquo;s Photos</span></a></h1>
-<!-- <nav>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-</nav> -->
+<div class="container">
 
+    <h1><a href="/"><div class="me"></div><span>Jack&rsquo;s Photos</span></a></h1>
+
+    <ul class="social">
+        <li>
+            <a href="https://instagram.com/jackdeadman96">
+                <img src="/insta.png" alt="Instagram logo">
+            </a>
+        </li>
+    </ul>
+</div>
 
 <style>
 
+    .container {
+        display: flex;
+        align-items: baseline;
+        justify-content: space-between;
+    }
+
+    .social {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+
+    h1 {
+        margin-bottom: 0;
+    }
+
+    .social img {
+        width: 40px;
+        height: 40px;
+    }
 
     h1 .me {
         display: inline-block;
