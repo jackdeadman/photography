fb68d4924fa8025aba3b2a075f5b5ab56f7ddc6f
jackdeadman1@gmail.com <jackdeadman1@gmail.com>
2021-07-31 23:01:42 +0100

    Maybe form working

---
+++ src/routes/contact_success.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+	export const prerender = true;
+    export const hydrate = false;
+</script>
+
+Thank you for contacting me. I'll get back to you ASAP!
+<style>
+
+    label span {
+        width: 100px;
+        display: inline-block;
+    }
+
+    textarea {
+        font-size: 1rem;
+        width: 400px;
+        min-height: 400px;
+    }
+</style>
