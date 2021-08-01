fc47f12f1c513fa600385c2c06b141ea0b2f494c
jackdeadman1@gmail.com <jackdeadman1@gmail.com>
2021-08-01 12:35:08 +0100

    Maybe better rss

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,6 +6,13 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+
+    <link
+        type="application/atom+xml"
+        rel="alternate"
+        href="/rss.xml"
+        title="Jack Deadman Photography"
+    />
 </svelte:head>
 
 <script>
