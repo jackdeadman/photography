777c98276387e0fb8099ca901bcb025742145ffd
jackdeadman1@gmail.com <jackdeadman1@gmail.com>
2021-08-01 11:34:29 +0100

    tags

--- bin/compile-gallery-json.js
+++ bin/compile-gallery-json.js
@@ -7,15 +7,31 @@ try {
     // Add redundancy but allows for fast lookup
     let output = {
         photos: [],
-        slugs: {}
+        slugs: {},
+        filters: {}
     };
+
+    
+    output.filters.tags = new Set([])
+    output.filters.cameras = new Set([])
+    output.filters.lenses = new Set([])
 
 	glob('data/gallery/*.yaml', (err, matches) => {
 	  for (let match of matches) {
 			const doc = yaml.load(fs.readFileSync(match, 'utf8'));
 			output.slugs[doc.slug] = doc;
+            for (let tag in doc.tags) {
+                output.filters.tags.add(tag);
+            }
+
+            output.filters.cameras.add(doc.meta.image.Model)
+            output.filters.lenses.add(doc.meta.exif.LensModel)
             output.photos.push(doc);
 		}
+
+        output.filters.cameras = Array.from(output.filters.cameras);
+        output.filters.tags = Array.from(output.filters.tags);
+        output.filters.lenses = Array.from(output.filters.lenses);
 
         fs.writeFileSync('src/lib/photos.json', JSON.stringify(output, null, 4));
 	});
--- src/routes/_photo_gallery.svelte
+++ src/routes/_photo_gallery.svelte
@@ -1,37 +1,119 @@
 <script>
+
+    import { goto } from '$app/navigation';
+    import { page } from '$app/stores';
     export let photos;
 
     function formatDate(time) {
         const [year, month, day] = time.split(' ')[0].split(':');
         return `${day}/${month}/${year}`;
     }
+
+    function formatTag(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    function hasTag(tag, query) {
+        const currentTags = query.getAll('tag');
+        return currentTags.indexOf(tag) !== -1;
+    }
+
+    function setTag(tag) {
+        const query = new URLSearchParams($page.query.toString());
+
+        if (query.has('tag') && query.get('tag') == tag) {
+            query.delete('tag');
+        } else {
+            query.delete('tag');
+            query.append('tag', tag);
+        }
+        goto($page.path + '?' + query.toString())
+    }
+
+    function addTag(tag) {
+        const query = new URLSearchParams($page.query.toString());
+        const currentTags = query.getAll('tag');
+
+        if (currentTags.indexOf(tag) == -1) {
+            query.append('tag', tag);
+        } else {
+            query.delete('tag');
+            const newTags = currentTags.filter(t => t !== tag);
+            newTags.forEach(t => {
+                query.append('tag', t);
+            })
+        }
+
+        goto($page.path + '?' + query.toString())
+    }
+
 </script>
 <h2 class="title">Gallery</h2>
 
 <div class="container">
     {#each photos as photo (photo.slug)}
+        {#if !$page.query.get('tag') || photo.tags.includes($page.query.get('tag'))}
         <div class="img-card">
-            <a sveltekit:prefetch href={`/photo/${photo.slug}`}>
-                <figure>
-                    <picture>
-                        <source srcset={photo.versions.small} type="image/webp">
-                        <source srcset={photo.versions.small_jpg} type="image/jpeg"> 
-                        <img src={photo.versions.small_jpg} alt={photo.alt}>
-                    </picture>
-                </figure>
-            </a>
+            <div>
+                <a sveltekit:prefetch href={`/photo/${photo.slug}`}>
+                    <figure>
+                        <picture>
+                            <source srcset={photo.versions.small} type="image/webp">
+                            <source srcset={photo.versions.small_jpg} type="image/jpeg"> 
+                            <img src={photo.versions.small_jpg} alt={photo.alt}>
+                        </picture>
+                    </figure>
+                </a>
+            </div>
+            
 
             <div class="info">
                 <h2><a href={`/photo/${photo.slug}`}>{photo.name}</a></h2>
                 <p>{photo.description}</p>
                 <date>{formatDate(photo.meta.exif.DateTimeOriginal)}</date>
+
+                <ul class="tags">
+                    {#each photo.tags as tag (tag)}
+                        <li class:active={hasTag(tag, $page.query)}><a on:click|preventDefault={() => setTag(tag)} href="#">{formatTag(tag)}</a></li>
+                    {/each}
+                </ul>
             </div>
         </div>
+        {/if}
     {/each}
 </div>
 
 
 <style>
+
+    .tags {
+        margin: 5px 0;
+        padding: 0;
+    }
+    
+    .tags a {
+        text-decoration: none;
+    }
+
+    .tags li {
+        list-style: none;
+        display: inline-block;
+        padding: 5px 10px;
+        border-radius: 5px;
+        background-color: #fff;
+        border: 1px solid #ccc;
+        color: #222;
+        margin: 5px 10px 5px 0;
+    }
+
+    .tags li.active {
+        background-color: #222;
+        color: #ddd;
+    }
+
+    .tags li.active a {
+        color: #ddd;
+    }
 
     h2.title {
         margin: 0;
